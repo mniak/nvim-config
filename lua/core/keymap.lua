831f3d1c8057273ba0cc253b9a6e844d24798d6e
Andre Soares <andregsilv@gmail.com>
2023-08-01 22:45:50 -0300

    keys for dap

--- lua/core/keymap.lua
+++ lua/core/keymap.lua
@@ -3,6 +3,23 @@
 vim.keymap.set('n', '<c-p>', require('telescope.builtin').find_files, { desc = "Go to file" })
 vim.keymap.set('n', '<c-s-p>', require("telescope.builtin").commands, { desc = "Show commands" })
 
+vim.keymap.set('n', '<c-q>', "<cmd>qall!<cr>", { desc = "Force close all buffers" })
 
-vim.keymap.set('n', '<f9>', "<cmd>GoBreakToggle<cr>", { desc = "Go: Toggle breakpoint" })
-vim.keymap.set('n', '<f5>', "<cmd>GoDebug<cr>", { desc = "Go: Start debug session" })
+local dap = require('dap')
+local godap = require('go.dap')
+local dapui = require('dapui')
+
+vim.keymap.set('n', '<f9>', dap.toggle_breakpoint, { desc = "Go: Toggle breakpoint" })
+vim.keymap.set('n', '<f5>', function()
+  if dap.session() then
+    dap.continue()
+  else
+    godap.run()
+  end
+end, { desc = "Go: Start or continue debug session" })
+vim.keymap.set('n', '<f10>', dap.step_over, { desc = "DAP: Step over" })
+vim.keymap.set('n', '<f11>', dap.step_into, { desc = "DAP: Step into" })
+vim.keymap.set('n', '<s-f11>', dap.step_out, { desc = "DAP: Step into" })
+vim.keymap.set('n', '<c-s-d>', dapui.toggle, { desc = "Special start" })
+
+vim.keymap.set('n', '<f1>', "55G<cmd>GoBreakToggle<cr><f5>", { desc = "Special start" })
